cb3133e1d3d8eecfd75f04a94bbb04fb947d31c4
gopikrishna000 <76482198+gopikrishna000@users.noreply.github.com>
2025-02-28 02:25:41 +0530

    Update content.js removed dynamic js injection logic

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 // Wait for the page to fully load
-window.addEventListener("load", function () {
+document.addEventListener("DOMContentLoaded", function() {
     let observer = new MutationObserver((mutations, obs) => {
         let activityBox = document.querySelector("#pageContent");
         if (activityBox) {
@@ -13,20 +13,6 @@ window.addEventListener("load", function () {
             heatmapContainer.style.visibility = "hidden"; // Hide until fully loaded
             // Insert the container after the activity box
             activityBox.parentNode.appendChild(heatmapContainer);
-
-            // Inject D3.js
-            let d3Script = document.createElement("script");
-            d3Script.src = chrome.runtime.getURL("libs/d3.min.js");
-            d3Script.onload = () => {
-                console.log("D3.js loaded");
-
-                // Inject heatmap.js
-                let script = document.createElement("script");
-                script.src = chrome.runtime.getURL("heatmap.js");
-                script.type = "module";
-                document.body.appendChild(script);
-            };
-            document.body.appendChild(d3Script);
         }
     });
 
